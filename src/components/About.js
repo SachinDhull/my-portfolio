0b382b788bf6a752a146769047f987a1635fa596
Sachin Dhull <sachindhull916@gmail.com>
2023-05-25 14:25:44 +0530

    Added links to projects

--- src/components/About.js
+++ src/components/About.js
@@ -14,13 +14,11 @@ const About = () => {
           </div>
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
             <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi. I'm Sai Kumar, nice to meet you. Please take a look around.</p>
+              <p>Hi. I'm Sachin Dhull, nice to meet you. Please take a look around.</p>
             </div>
             <div>
-              <p>I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
+              <p>Welcome to my portfolio! I am a passionate and driven Front End Developer,
+                 ready to embark on an exciting journey of creating dynamic and intuitive user interfaces. With a solid foundation in web development and a keen eye for design, I strive to build engaging experiences that bring ideas to life. . What would you do if you had
               a software expert available at your fingertips?</p>  
             </div>
           </div>
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import Working from'../assests/workImg.jpg'
-import realEstate from'../assests/realestate.jpg'
+import Disney from'../assests/disney.png'
+import Netflix from'../assests/netflix.png'
+import Youtube from '../assests/youtube.png'
+import Game from '../assests/fighter.png'
+import Port from '../assests/portfolio.png'
 const Work = () => {
   return (
     <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
@@ -10,22 +13,76 @@ const Work = () => {
         <p className='py-6'>// Check out some of my recent work</p>
        </div>
 
-       {/* container */}4
+       {/* container */}
 
         <div 
         className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
           {/* grid Item */}
-            <div style={{backgroundImage:`url(${Working})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            <div style={{backgroundImage:`url(${Disney})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                {/* hover */}
 
                
                 <div className='opacity-0 group-hover:opacity-100'>
                     <span className='text-2xl font-bold text-white tracking-wider'>
-                      React Js Application
+                      Disney+ Clone using Reactjs
                     </span>
                     <div className='pt-8 text-center'>
-                        <a href='/'>
+                        <a href='https://sachindhull.github.io/hotstar-clone/'>
+                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                        </a>
+                        <a href='https://github.com/SachinDhull/hotstar-clone'>
+                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>code</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div style={{backgroundImage:`url(${Netflix})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+               {/* hover */}
+
+               
+                <div className='opacity-0 group-hover:opacity-100'>
+                    <span className='text-2xl font-bold text-white tracking-wider'>
+                    Netflix Clone using Reactjs
+                    </span>
+                    <div className='pt-8 text-center'>
+                        <a href='https://sachindhull.github.io/netflix-clone/'>
+                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                        </a>
+                        <a href='https://github.com/SachinDhull/netflix-clone'>
+                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>code</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div style={{backgroundImage:`url(${Youtube})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+               {/* hover */}
+
+               
+                <div className='opacity-0 group-hover:opacity-100'>
+                    <span className='text-2xl font-bold text-white tracking-wider'>
+                      Youtube Home page html & Css
+                    </span>
+                    <div className='pt-8 text-center'>
+                        <a href='https://sachindhull.github.io/youtube-homepage-ui/'>
+                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                        </a>
+                        <a href='https://github.com/SachinDhull/youtube-homepage-ui'>
+                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>code</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div style={{backgroundImage:`url(${Game})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+               {/* hover */}
+
+               
+                <div className='opacity-0 group-hover:opacity-100'>
+                    <span className='text-2xl font-bold text-white tracking-wider'>
+                      javascript street fighter game
+                    </span>
+                    <div className='pt-8 text-center'>
+                        <a href='https://street-fighter.sachindhull1.repl.co/'>
                             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                         </a>
                         <a href='/'>
@@ -34,79 +91,25 @@ const Work = () => {
                     </div>
                 </div>
             </div>
-            <div style={{backgroundImage:`url(${realEstate})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            <div style={{backgroundImage:`url(${Port})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                {/* hover */}
 
                
                 <div className='opacity-0 group-hover:opacity-100'>
                     <span className='text-2xl font-bold text-white tracking-wider'>
-                      React Js Application
+                      Portfolio code
                     </span>
                     <div className='pt-8 text-center'>
-                        <a href='/'>
+                        {/* <a href='/'>
                             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                        </a>
-                        <a href='/'>
+                        </a> */}
+                        <a href='https://github.com/SachinDhull/my-portfolio'>
                             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>code</button>
                         </a>
                     </div>
                 </div>
             </div>
-            <div style={{backgroundImage:`url(${Working})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-               {/* hover */}
-
-               
-                <div className='opacity-0 group-hover:opacity-100'>
-                    <span className='text-2xl font-bold text-white tracking-wider'>
-                      React Js Application
-                    </span>
-                    <div className='pt-8 text-center'>
-                        <a href='/'>
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                        </a>
-                        <a href='/'>
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>code</button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div style={{backgroundImage:`url(${realEstate})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-               {/* hover */}
-
-               
-                <div className='opacity-0 group-hover:opacity-100'>
-                    <span className='text-2xl font-bold text-white tracking-wider'>
-                      React Js Application
-                    </span>
-                    <div className='pt-8 text-center'>
-                        <a href='/'>
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                        </a>
-                        <a href='/'>
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>code</button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div style={{backgroundImage:`url(${Working})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-               {/* hover */}
-
-               
-                <div className='opacity-0 group-hover:opacity-100'>
-                    <span className='text-2xl font-bold text-white tracking-wider'>
-                      React Js Application
-                    </span>
-                    <div className='pt-8 text-center'>
-                        <a href='/'>
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                        </a>
-                        <a href='/'>
-                            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>code</button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div style={{backgroundImage:`url(${realEstate})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+            <div style={{backgroundImage:`url(${Disney})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                {/* hover */}
 
                
